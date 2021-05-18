d29082c2345e6ddb6db1e356dee2d953ec773507
Staffan Aberg <'staffan.aaberg@gmail.com'>
2021-05-18 12:22:13 +0200

    What is this?

--- forkExample/src/main/java/common/Triangle.java
+++ forkExample/src/main/java/common/Triangle.java
@@ -14,16 +14,5 @@ public class Triangle {
 	//This class needs to be implemented
 	
 
-	public int getAngle(int i) {
-		int angle = 0;
-		if(i==0) {
-			angle = 1;
-		} else if(i==1) {
-			angle = 2;
-		} else {
-			angle = 0;
-		}
-		
-		return 30*angle;
-	}
+	//what is this?
 }
